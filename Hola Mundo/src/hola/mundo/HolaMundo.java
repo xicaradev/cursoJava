5e3098992a388e4c8442a900e2d1ef70b5efc55e
xicaradev <xicaradev@gmail.com>
2023-02-15 20:22:37 -0600

    Subiendo el Hola Mundo

---
+++ Hola Mundo/src/hola/mundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+/**
+ *
+ * @author eduardoxicara
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Mi primer hola Mundo en Java
+        System.out.println("Hola Mundo");
+    }
+    
+}
