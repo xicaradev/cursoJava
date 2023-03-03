1c4a954b58fcd74f5ed2cec40ab6eb8166cc5f6d
xicaradev <xicaradev@gmail.com>
2023-03-02 20:09:18 -0600

    Ejercicio 1 de Guía Intro Java

---
+++ EjerciciosJava/src/Ejercicio01.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author eduardoxicara
+ */
+public class Ejercicio01 {
+    
+    public static void main(String[] args) {
+    
+        byte numero = (byte) 1000;
+        System.out.println(numero);
+        
+        long numero_grande = (long) 1000;
+        System.out.println(numero_grande);
+        
+        String nombre = "Eduardo";
+        System.out.println(nombre);
+        
+        float decimal = (float) 3.5;
+        System.out.println(decimal);
+        
+        boolean decision = false;
+        System.out.println(decision);
+        
+        int number = 10;
+        System.out.println(number);
+        
+        
+    }
+    
+}
